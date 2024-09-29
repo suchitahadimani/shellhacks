335b9f4736558d2b04d51bc67b6040ca1ae74a0d
suchitahadimani <suchita.careers@gmail.com>
2024-09-29 07:52:49 -0400

    checking nfc4

--- nextjs-blog/src/app/api/nfcApi.js
+++ nextjs-blog/src/app/api/nfcApi.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
 export const sendDataToApi = async (data) => {
     const prompt = `If serial number for the following NFC card reading is not null, return in a direct response which credit card would be the best for buying gas: ${JSON.stringify(data)}`;
     const apiKey = process.env.PERPLEXITY_API;
+
+
 
     const m_body = {
         model: "llama-3.1-sonar-small-128k-online",
@@ -16,7 +20,7 @@ export const sendDataToApi = async (data) => {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${PERPLEXITY_API}`, 
+                'Authorization': `Bearer ${apiKey}`, 
             },
             body: JSON.stringify(m_body) 
         });
@@ -29,7 +33,7 @@ export const sendDataToApi = async (data) => {
 
         return await response.json();
     } catch (error) {
-        alert('Debug info: ' + JSON.stringify(PERPLEXITY_API) + JSON.stringify(apiKey));
+        alert('Debug info: ' + JSON.stringify(apiKey));
 
         throw new Error(error.message);
     }
